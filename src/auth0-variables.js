fa9e58c6e5e08ae183f6e8d3e972c14805d2a2e9
react-web204 <58696730+react-web204@users.noreply.github.com>
2020-01-09 23:50:53 +0800

    rbac_app by dragon.

---
+++ src/auth0-variables.js
@@ -0,0 +1,6 @@
+export const AUTH_CONFIG = {
+    domain: "dev-7queur4s.auth0.com",
+    roleUrl: "https://rbac-tutorial-app/role",
+    clientId: "qoiiC1gRUEWnqeUA24ZOwLTremcxrIEG",
+    callbackUrl: "http://localhost:3000/callback"
+  };
